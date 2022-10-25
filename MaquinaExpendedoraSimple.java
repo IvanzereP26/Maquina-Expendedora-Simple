93d681ee31c58960ce702db812b79a4e257124b5
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-10-25 09:14:54 +0200

    Añadiendo segundo constructor

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,6 +23,13 @@ public class MaquinaExpendedoraSimple {
         estacionOrigen = "León";
         estacionDestino = "Sevilla";
     }
+    public MaquinaExpendedoraSimple(int precioDeBillete, String destino) {
+        precioBillete = precioDeBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "León";
+        estacionDestino = "destino";
+    }
 
     /**
      * Devuelve el precio del billete
